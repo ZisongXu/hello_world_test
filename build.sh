4fbee3404c6fbe72b1f9fbd6b7e7f2e9ee4922c2
Zisong <1210652636@qq.com>
2022-05-13 11:03:55 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo singularity build \
+    --sandbox \
+    ubuntu16.04_kinetic_container \
+    ubuntu16_test5.def
+
+# Usually the home/.bashrc will include user-specific changes and we don't want
+# to track them. For example, having ROS_IP to their IP address to work over
+# the network. The following command will tell git to ignore changes to that
+# file henceforth.
+# git update-index --assume-unchanged home/.bashrc
