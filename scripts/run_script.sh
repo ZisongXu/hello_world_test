705f542fbaaa157ab5a1f53b31a2365ef8152a8a
Zisong <1210652636@qq.com>
2022-05-17 13:57:02 +0100

    2022_5_17_1

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -6,6 +6,10 @@ if [ ! -d "$HOME/catkin_ws/devel" ]; then
     cd $HOME/catkin_ws/
     catkin_make
     pip install pyquaternion
+    pip3 install pybullet
+    wget https://bootstrap.pypa.io/pip/3.5/get-pip.py
+    python3 get-pip.py
+    pip3 install numpy
 fi
 
 source $HOME/catkin_ws/devel/setup.bash
